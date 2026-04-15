ed16f79320d19f5623f964e913dd6c63fe0d9baf
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-04-15 12:31:18 -0400

    Modernize type hints for organization.py

--- mytypes.py
+++ mytypes.py
@@ -3,10 +3,8 @@
 Created 7 Mar 2023 by Greg Vance
 """
 
-import numpy as np
 from scipy import optimize
 
 
-type Array = np.ndarray
-type Choices = list[str] | list[None] | list[str | None]
+type Choices = list[str | None]
 type Result = optimize.OptimizeResult
